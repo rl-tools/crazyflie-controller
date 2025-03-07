a418a39e91c11151d61861dc0b5141ebedf8609f
Jonas Eschmann <jonas.eschmann@gmail.com>
2025-03-06 21:20:58 -0500

    bump firmware (disabling filters); good policy, does figure eight in 3.25 but wobbles a bit

--- rl_tools_adapter.cpp
+++ rl_tools_adapter.cpp
@@ -32,6 +32,7 @@ using T = typename ACTOR_TYPE::SPEC::T;
 constexpr TI CONTROL_FREQUENCY_MULTIPLE = 5; // CONTROL_INTERVAL_MS = 2 => 500 Hz => CONTROL_FREQUENCY_MULTIPLE = 5 (to match the 100 Hz of training)
 static TI controller_tick = 0;
 constexpr TI ACTION_HISTORY_LENGTH = 16; //rlt::checkpoint::environment::ACTION_HISTORY_LENGTH
+
 #ifdef RL_TOOLS_ACTION_HISTORY
 static constexpr TI INPUT_DIM = rlt::get_last(ACTOR_TYPE::INPUT_SHAPE{});
 static constexpr TI OUTPUT_DIM = rlt::get_last(ACTOR_TYPE::OUTPUT_SHAPE{});
